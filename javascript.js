708ed6841bd08b7246316248f5c6c3b490fca471
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-02-10 14:10:19 +0000

    added top row as user's timezone
    second row as a default city but also allow user to select by using select form.

--- javascript.js
+++ javascript.js
@@ -2,13 +2,17 @@ function displayChosenTimezone(event) {
   let userChosenTimeZoneValue = event.target.value;
   if (userChosenTimeZoneValue.length > 0) {
     let now = moment().tz(`${userChosenTimeZoneValue}`);
-    let timezoneTime = now.format("dddd, MMMM D, HH:mm:ss");
+    let timezoneTime = now.format("HH:mm:ss");
+    let timezoneDate = now.format("ddd, D MMM YYYY");
 
     let chosenCity = document.querySelector("#chosenCity");
     chosenCity.innerHTML = `${userChosenTimeZoneValue}`;
 
-    let chosenCityDate = document.querySelector("#chosenCityDate");
-    chosenCityDate.innerHTML = `${timezoneTime} `;
+    let chosenCityTime = document.querySelector("#chosenCityTime");
+    chosenCityTime.innerHTML = `${timezoneTime} `;
+
+    let chosenCityDate = document.querySelector("#chosenCitytDate");
+    chosenCityDate.innerHTML = `${timezoneDate}`;
   }
 }
 
@@ -20,9 +24,31 @@ function showUserCurrentTimezone() {
   let userCity = document.querySelector("#userCurrentTimezone");
   userCity.innerHTML = `${userCurrentCity}`;
   let userTime = document.querySelector("#userCurrentTime");
-  userTime.innerHTML = moment()
+  userTime.innerHTML = moment().tz(`${userCurrentCity}`).format("HH:mm:ss");
+  let userDate = document.querySelector("#userCurrentDate");
+  userDate.innerHTML = moment()
     .tz(`${userCurrentCity}`)
-    .format("dddd, MMMM D, HH:mm:ss");
+    .format("ddd, D MMM YYYY");
 }
 
 setInterval(showUserCurrentTimezone, 1000);
+
+let defaultCity = "Asia/Hong_Kong";
+
+let defaultCityName = document.querySelector("#chosenCity");
+defaultCityName.innerHTML = `${defaultCity}`;
+
+let defaultCityTime = document.querySelector("#chosenCityTime");
+defaultCityTime.innerHTML = moment().tz(`${defaultCity}`).format("HH:mm:ss");
+
+let defaultCityDate = document.querySelector("#chosenCitytDate");
+defaultCityDate.innerHTML = moment()
+  .tz(`${defaultCity}`)
+  .format("ddd, D MMM YYYY");
+
+function changeTimeFormat(event) {
+  let userTime = document.querySelector("#userCurrentTime");
+  userTime.innerHTML = moment().tz(`${userCurrentCity}`).format("hh:mm:ss A");
+}
+let timeFormatButton = document.querySelector(".amPmbutton");
+timeFormatButton.addEventListener("click", changeTimeFormat);
